4701bec585e6c870841215d63161b49d7f5127b4
Usher Missiedjan <531498@student.fontys.nl>
2025-05-06 10:11:47 +0200

    Created car mock repository and unit test class.

---
+++ OnderhoudsbuddyDotnet/UnitTests/MockRepository/CarMockRepository.cs
@@ -0,0 +1,18 @@
+﻿using Domain.RepositoryInterfaces;
+using Moq;
+
+namespace UnitTests.MockRepository;
+
+public class CarMockRepository 
+{
+    private readonly Mock<ICar> _carRepository;
+    public CarMockRepository()
+    {
+        _carRepository = new Mock<ICar>();
+    }
+    public Mock<ICar> GetCarRepository()
+    {
+        return _carRepository;
+    }
+    
+}
---
+++ OnderhoudsbuddyDotnet/UnitTests/Test/CarContainerTest.cs
@@ -0,0 +1,34 @@
+﻿using Business.Containers;
+using UnitTests.MockRepository;
+using Xunit;
+
+namespace UnitTests.Test;
+
+public class CarContainerTest
+{
+    private readonly CarContainer _carContainer;
+    private readonly CarMockRepository _mockRepository;
+
+    public CarContainerTest()
+    {
+        _mockRepository = new CarMockRepository();
+        _carContainer = new CarContainer(_mockRepository.GetCarRepository().Object);
+        
+        [Fact]
+        public void GetCarAsyncTest()
+        {
+            //Act 
+            
+            //Arrange
+            
+            //Assert
+        }
+        
+        
+        
+        
+        
+        
+    }
+
+}
