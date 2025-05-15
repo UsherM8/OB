f79f165f20265ee9c4e7f895bb67a5f4445bcf05
Usher Missiedjan <531498@student.fontys.nl>
2025-05-15 11:36:05 +0200

    Added user dto and serviceinterfaces.

--- OnderhoudsbuddyJava/src/main/java/org/example/onderhoudsbuddyjava/domain/dto/UserDto.java
+++ OnderhoudsbuddyJava/src/main/java/org/example/onderhoudsbuddyjava/domain/dto/UserDto.java
@@ -1,4 +1,23 @@
 package org.example.onderhoudsbuddyjava.domain.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserDto {
+    private Integer userId;
+    private String firstName;
+    private String lastName;
+    private int birthDate;
+    private String email;
+    private String type;
+
+
 }
--- OnderhoudsbuddyJava/src/main/java/org/example/onderhoudsbuddyjava/domain/serviceinterfaces/IUserService.java
+++ OnderhoudsbuddyJava/src/main/java/org/example/onderhoudsbuddyjava/domain/serviceinterfaces/IUserService.java
@@ -1,4 +1,12 @@
 package org.example.onderhoudsbuddyjava.domain.serviceinterfaces;
 
+import org.example.onderhoudsbuddyjava.domain.dto.UserDto;
+import java.util.List;
+
 public interface IUserService {
+    List<UserDto> getAllUsers();
+    UserDto getUserById(Integer id);
+    UserDto createUser(UserDto userDto, String password);
+    UserDto updateUser(UserDto userDto);
+    Boolean deleteUser(Integer id);
 }
