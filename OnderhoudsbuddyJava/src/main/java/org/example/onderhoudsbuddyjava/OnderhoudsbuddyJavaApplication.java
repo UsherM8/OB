33bae68090c88869ac25e9f5af064975e2803ecc
Usher Missiedjan <531498@student.fontys.nl>
2025-05-15 11:19:11 +0200

    Added onderhoudsbuddyjava project

---
+++ OnderhoudsbuddyJava/src/main/java/org/example/onderhoudsbuddyjava/OnderhoudsbuddyJavaApplication.java
@@ -0,0 +1,13 @@
+package org.example.onderhoudsbuddyjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnderhoudsbuddyJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnderhoudsbuddyJavaApplication.class, args);
+    }
+
+}
