7b0411449070b6ae87f6e0db22aefd7104a72f17
Usher Missiedjan <531498@student.fontys.nl>
2025-06-11 16:02:40 +0200

    Made docker files and container owrk for java and dotnet  backend

--- OnderhoudsbuddyDotnet/OnderhoudsbuddyWeb/Program.cs
+++ OnderhoudsbuddyDotnet/OnderhoudsbuddyWeb/Program.cs
@@ -23,16 +23,17 @@ builder.Services.AddScoped<IService, ServiceRepository>();
 builder.Services.AddScoped<IServiceContainer, ServiceContainer>();
 builder.Services.AddScoped<ServiceMapper>();
 
+builder.Configuration
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true)
+    .AddEnvironmentVariables();
+
+
 var env = builder.Environment.EnvironmentName;
 Console.WriteLine($"Environment: {env}");
 
-var config = builder.Configuration
-    .SetBasePath(Directory.GetCurrentDirectory())
-    .AddJsonFile("appsettings.json", optional: false)
-    .AddJsonFile($"appsettings.{env}.json", optional: true)
-    .Build();
-
-var connectionString = config.GetConnectionString("DefaultConnection");
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
 builder.Services.AddDbContext<OnderhoudsbuddyDbContext>(options =>
     options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
@@ -50,13 +51,23 @@ builder.Services.AddCors(options =>
     });
 });
 
+
+
 builder.Services.AddHttpClient<RdwApiClient>(client =>
 {
     client.BaseAddress = new Uri("https://opendata.rdw.nl/resource/");
     client.DefaultRequestHeaders.Add("Accept", "application/json");
 });
 
+
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<OnderhoudsbuddyDbContext>();
+    db.Database.Migrate();
+}
+
 
 if (app.Environment.IsDevelopment() || app.Environment.IsEnvironment("Test"))
 {
